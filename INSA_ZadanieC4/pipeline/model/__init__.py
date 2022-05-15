d9cb6a4b436c46d0b156026bce53926f1f7f54ea
SmolkoMatus <71692364+SmolkoMatus@users.noreply.github.com>
2022-05-15 20:12:54 +0200

    Delete INSA_ZadanieC4 directory

--- INSA_ZadanieC4/pipeline/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from model import config
-from model import manager
-from model import pipeline
-from model import predict
-from model import inputers
-from model import validation
--- INSA_ZadanieC4/pipeline/run_tests.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-
-if __name__ == '__main__':
-  path = pathlib.Path(__file__).resolve().parent
-  path /= '.\\.virenv\\Scripts\\tox.exe'
-  path = f"\"{path}\""
-  os.system(path)
